6368aaf527217fbe83fb93fbb0321b42f873e8dc
Youssef <youssefbarsoom@gmail.com>
2021-09-25 11:08:57 +0200

    first commit

--- RescueSimulation/src/model/events/SOSResponder.java
+++
@@ -1,9 +0,0 @@
-package model.events;
-
-import exceptions.SimulationException;
-import exceptions.UnitException;
-import simulation.Rescuable;
-
-public interface SOSResponder {
-public void respond(Rescuable r) throws SimulationException;
-}
